4be568cba855f59fbc24572c3b83fcd74987d7ec
mymywas <mynvt95@gmail.com>
2017-11-25 13:20:12 +0700

    no message

--- js/CV.js
+++ js/CV.js
@@ -3,5 +3,19 @@
  * Created by MYNVTSE61526 on 06/11/2017.
  */
 $( document ).ready(function() {
-   $(".cv-block").css('min-height', $(window).height())
+   // $(".cv-block").css('min-height', $(window).outerHeight());
+    var screenheight = $(window).width();
+    if(screenheight >= 1240){
+        $(".cv-block").css('min-height', $(window).outerHeight());
+    }else{
+        $(".cv-block").css('min-height', $(".cv-content").outerHeight());
+    }
+    $(window).on('resize', function(){
+        if($(this).width() >= 1240){
+            $(".cv-block").css('min-height', $(window).outerHeight());
+        }else{
+            $(".cv-block").css('min-height', $(".cv-content-block").outerHeight());
+        }
+
+    });
 });
